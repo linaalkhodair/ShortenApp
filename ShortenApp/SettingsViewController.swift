11229042fec085d18755567bdc06504b263dc08b
Lina Alkhodair <linaalkhodair9@gmail.com>
2020-07-23 11:10:02 +0300

    settings ui

---
+++ ShortenApp/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  ShortenApp
+//
+//  Created by Lina Alkhodair on 23/07/2020.
+//  Copyright © 2020 Lina Alkhodair. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
